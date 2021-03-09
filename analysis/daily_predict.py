6a3347ea45906458c7bef03e7d6deb6de97e9116
stefanbo92 <stefan.boschenriedter@gmx.de>
2021-03-09 22:42:07 +0100

    enhancing daily predict

--- analysis/daily_predict.py
+++ analysis/daily_predict.py
@@ -13,6 +13,7 @@ from feature_engineering import *
 from get_train_data import *
 
 from datetime import datetime
+import time
 
 
 def get_latest_news(news_sites, buy_time):
@@ -80,7 +81,41 @@ def get_fin_data(stock, buy_time):
     return np.reshape(numeric_features, (1,-1))
 
 
-def daily_predict(buy_time, stock, news_sites):
+def train_classifier(buy_time, stock, news_sites):
+    
+    # getting train features and labels from file
+    text_features, numeric_features, lables, bin_labels = get_prepared_data()
+    if(len(text_features)==0):
+        return None, None, None
+
+    # creating CountVectorizer
+    countVector = CountVectorizer(ngram_range=(2, 2))
+    # ngram(2,2) means it will combine the 2 words together and assign the value
+    trainDataset = countVector.fit_transform(text_features)
+    trainDataset = trainDataset.toarray()
+
+    # dropping features with low occurence
+    drop_thresh = 3
+    drop_idx = []
+    for col_idx in range(trainDataset.shape[1]):
+        curr_col = trainDataset[:, col_idx]
+        if(np.sum(curr_col) < drop_thresh):
+            drop_idx.append(col_idx)
+    print("dropping", len(drop_idx), "features of", trainDataset.shape[1])
+    trainDataset = np.delete(trainDataset, drop_idx, 1)
+
+    # adding numerical features
+    add_numerical = True
+    if add_numerical:
+        trainDataset = np.append(trainDataset, numeric_features ,axis=1)
+
+    # training random forest classifier
+    xgb_classifier = XGBClassifier()
+    xgb_classifier.fit(trainDataset, bin_labels)
+
+    return xgb_classifier, countVector, drop_idx
+
+def daily_predict(xgb_classifier, countVector, drop_idx, buy_time, stock, news_sites):
     # getting current date and time
     today = datetime.now()
     date_time = today.strftime("%Y_%m_%d_%H_%M")
@@ -96,46 +131,67 @@ def daily_predict(buy_time, stock, news_sites):
     # getting latest news
     latest_news = get_latest_news(news_sites, buy_time)
 
-    # creating CountVectorizer
-    countVector = CountVectorizer(ngram_range=(2, 2))
-    # ngram(2,2) means it will combine the 2 words together and assign the value
-    trainDataset = countVector.fit_transform(text_features)
-    trainDataset = trainDataset.toarray()
-
     # prediction classifier
     predDataset = countVector.transform([latest_news])
     predDataset = predDataset.toarray()
 
     # dropping features with low occurence
-    drop_thresh = 3
-    drop_idx = []
-    for col_idx in range(trainDataset.shape[1]):
-        curr_col = trainDataset[:, col_idx]
-        if(np.sum(curr_col) < drop_thresh):
-            drop_idx.append(col_idx)
-    print("dropping", len(drop_idx), "features of", trainDataset.shape[1])
-    trainDataset = np.delete(trainDataset, drop_idx, 1)
     predDataset = np.delete(predDataset, drop_idx, 1)
 
     # adding numerical features
     add_numerical = True
     if add_numerical:
-        trainDataset = np.append(trainDataset, numeric_features ,axis=1)
         predDataset = np.append(predDataset, latest_numeric_feat ,axis=1)
-
-    # training random forest classifier
-    xgb_classifier = XGBClassifier()
-    xgb_classifier.fit(trainDataset, bin_labels)
 
     # predict daily
     prediction = xgb_classifier.predict(predDataset)
     print("todays prediction:", prediction)
+
+    return prediction
 
     
 
 hyp_param = [9, 13, "dax", ["cnbc_finance"]]
 # getting train data
 get_train_data(hyp_param[0], hyp_param[1], hyp_param[3], hyp_param[2])
-daily_predict(hyp_param[0], hyp_param[2], hyp_param[3])
+# train classifier
+xgb_classifier, countVector, drop_idx = train_classifier(hyp_param[0], hyp_param[2], hyp_param[3])
+
+# looping every day until buy time is reached
+buy_time = [22,34]
+sell_time = hyp_param[1]
+while(True):
+    # getting current date and time
+    today = datetime.now()
+    date_time = today.strftime("%Y_%m_%d_%H_%M")
+    date_time_arr = date_time.split("_")
+    print("getting datetime:", date_time)
+
+    # buying
+    print("buying if", int(buy_time[0]),"==", int(date_time_arr[3]),"and", int(
+        buy_time[1]), "==", int(date_time_arr[4]))
+    if (buy_time[0]-int(date_time_arr[3]) == 0) and (buy_time[1]-int(date_time_arr[4]) <= 0):
+        # make daily prediction
+        prediction = daily_predict(xgb_classifier, countVector, drop_idx, hyp_param[0], hyp_param[2], hyp_param[3])
+        if prediction[0] > 0:
+            # buy long
+            pass
+        else:
+            # buy short
+            pass
+        print("buying done, sleeping for 9999 seconds")
+        time.sleep(9999) # sleep for two hours
+
+    # selling
+    print("selling if",sell_time,"==", int(date_time_arr[3]))
+    if sell_time-int(date_time_arr[3]) == 0:
+        # selling position
+        time.sleep(9999) # sleep for two hours
+
+    # wait and loop
+    print("sleeping for 30s")
+    time.sleep(30)
+
+
 
 
